f2d00a3ff765505a09a83d4adefbed9848fff0a2
pmartin <contact@elol.fr>
2016-04-28 10:50:55 +0200

    Add staffId to user info

--- src/user-service.ts
+++ src/user-service.ts
@@ -4,19 +4,23 @@ import {Injectable} from 'angular2/core';
 export class UserService {
   private LOGIN = 'login';
   private TOKEN = 'token';
+  private STFID = 'stfid';
 
-  connect(token, login) {
+  connect(token, login, stfId) {
     localStorage.setItem(this.TOKEN, token);
     localStorage.setItem(this.LOGIN, login);
+    localStorage.setItem(this.STFID, stfId);
   }
 
   getToken() { return localStorage.getItem(this.TOKEN); }
   getLogin() { return localStorage.getItem(this.LOGIN); }
+  getStaffId() { return localStorage.getItem(this.STFID); }
 
   isConnected() { return localStorage.getItem(this.TOKEN) !== null; }
 
   disconnect() {
     localStorage.removeItem(this.TOKEN);
     localStorage.removeItem(this.LOGIN);
+    localStorage.removeItem(this.STFID);
   }
 }
